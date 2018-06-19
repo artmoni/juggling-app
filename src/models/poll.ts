b4e9f5988a04dcba468f17cd764d762dce9cf7bb
sheshire <laurie.fernanli@gmail.com>
2018-06-19 13:35:27 +0200

    creation class Poll and Poll_answer

---
+++ src/models/poll.ts
@@ -0,0 +1,7 @@
+import {pollAnswer} from "./poll_answer";
+
+export class Poll {
+    question : string;
+    id : number;
+    answers : pollAnswer [];
+}
---
+++ src/models/poll_answer.ts
@@ -0,0 +1,4 @@
+export class pollAnswer {
+    id : number;
+    answer : string;
+}
